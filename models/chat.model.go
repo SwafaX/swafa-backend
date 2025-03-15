6e3c6e3abe257c817e6dcf249078273b61f37799
CalvinLe08 <lenguyenkhang102002@gmail.com>
2025-03-15 22:40:05 +0700

    fix stupid code from thinh

--- models/chat.model.go
+++ models/chat.model.go
@@ -3,14 +3,12 @@ package models
 import (
 	"github.com/google/uuid"
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type Chat struct {
-	ID           uuid.UUID  `gorm:"primaryKey" json:"id"`
-	Participant1 uuid.UUID 	`gorm:"not null" json:"participant1_id"`
-	Participant2 uuid.UUID  `gorm:"not null" json:"participant2_id"`
-	CreatedAt    time.Time 	`json:"created_at"`
-	UpdatedAt    time.Time 	`json:"updated_at"`
+	ID           uuid.UUID `gorm:"primaryKey" json:"id"`
+	Participant1 uuid.UUID `gorm:"not null" json:"participant1_id"`
+	Participant2 uuid.UUID `gorm:"not null" json:"participant2_id"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
