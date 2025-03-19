0ac45f61dd464e84a1865ec579a9bcbc6233b682
CalvinLe08 <lenguyenkhang102002@gmail.com>
2025-03-19 21:15:35 +0700

    reformat

--- models/message.model.go
+++ models/message.model.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Message struct {
-	ID        uuid.UUID      `gorm:"primaryKey" json:"id"`
-	ChatID    uuid.UUID     `gorm:"not null" json:"chat_id"`
-	SenderID  uuid.UUID      `gorm:"not null" json:"sender_id"`
+	ID        uuid.UUID `gorm:"primaryKey" json:"id"`
+	ChatID    uuid.UUID `gorm:"not null" json:"chat_id"`
+	SenderID  uuid.UUID `gorm:"not null" json:"sender_id"`
 	Content   string    `gorm:"type:text" json:"content"`
 	CreatedAt time.Time `json:"created_at"`
 }
