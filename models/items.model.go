d569bc0d30b43c297097bc335fd7f75d89ddae77
duckthihn <thinhpham1807@gmail.com>
2025-03-25 10:44:22 +0700

    Add image_url field to item

--- models/items.model.go
+++ models/items.model.go
@@ -1,8 +1,9 @@
 package models
 
 import (
-	"github.com/google/uuid"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Item struct {
@@ -10,6 +11,7 @@ type Item struct {
 	Title       string    `gorm:"size:255;not null" json:"title" form:"title" binding:"required"`
 	Description string    `gorm:"type:text" json:"description" form:"description"`
 	Status      string    `gorm:"size:50;not null;default:'unfinished'" json:"status" form:"status"`
+	ImageUrl    string    `gorm:"type:text" json:"image_url" form:"image_url"`
 	UserID      uuid.UUID `gorm:"type:uuid;not null" json:"user_id,omitempty"`
 	CreatedAt   time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt   time.Time `gorm:"autoUpdateTime" json:"updated_at"`
@@ -18,4 +20,5 @@ type Item struct {
 type ItemCreation struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
+	ImageUrl    string `json:"image_url"`
 }
